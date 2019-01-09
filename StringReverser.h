b4e654255bd4371586a8832b4aec281d383d47de
regevguy4 <45970256+regevguy4@users.noreply.github.com>
2019-01-09 18:37:08 +0200

    second upload.

---
+++ StringReverser.h
@@ -0,0 +1,21 @@
+#ifndef SECONDPART_STRINGREVERSER_H
+#define SECONDPART_STRINGREVERSER_H
+
+#include <iostream>
+#include <algorithm>
+
+#include "Solver.h"
+
+using namespace std;
+
+class StringReverser : public Solver<string, string> {
+public:
+    virtual string solve(const string &p) {
+        string solution = p;
+        reverse(solution.begin(), solution.end());
+        return solution;
+    }
+};
+
+
+#endif
